c76371167c6c813993496d00299c356a904ffe68
Eugene <gzkgt11@gmail.com>
2021-06-26 11:40:20 +0800

    add GET /api/quotes/random route

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.get("/api/quotes/random", (req, res, next) => {
+  res.send({ quote: getRandomElement(quotes) });
+});
 
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
