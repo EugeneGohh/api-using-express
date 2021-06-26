cb87bd7688f43a1bcf4c8c9ead2866a40db9ff29
Eugene <gzkgt11@gmail.com>
2021-06-26 21:57:22 +0800

    add POST & GET

--- server.js
+++ server.js
@@ -10,6 +10,37 @@ app.get("/api/quotes/random", (req, res, next) => {
   res.send({ quote: getRandomElement(quotes) });
 });
 
+app.get("/api/quotes", (req, res, next) => {
+  if (req.query.person !== undefined) {
+    const quotesByPerson = quotes.filter((quote) => {
+      quote.person === req.query.person;
+    });
+    res.send({
+      quotes: quotesByPerson,
+    });
+  } else {
+    res.send({
+      quotes: quotes,
+    });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person,
+  };
+
+  if (newQuote.quote && newQuote.person) {
+    quotes.push(newQuote);
+    res
+      .status(201)
+      .send({ quote: { quote: req.query.quote, person: req.query.person } });
+  } else {
+    res.status(400).send("Not found");
+  }
+});
+
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
